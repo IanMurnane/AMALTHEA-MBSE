4e9042b073ce94aa9cf0f3af961cdd2eec849bfe
Ian Murnane <ian.murnane@fh-dortmund.de>
2023-12-31 19:38:19 +0100

    Added 21 sensor input variables

--- src/app4mc/project/tool/java/Modules/InputVariables.java
+++ src/app4mc/project/tool/java/Modules/InputVariables.java
@@ -12,9 +12,29 @@ import org.eclipse.app4mc.amalthea.model.Label;
 
 public class InputVariables {
 	public static void run(Amalthea model, AmaltheaFactory factory) {
+		// Label - <size in bits>
 		Map<String, Integer> labelSizeMap = new HashMap<>();
-		labelSizeMap.put("MyLabel1", 16);
-		labelSizeMap.put("MyLabel2", 8);
+		labelSizeMap.put("FrontCameraA", 32);
+		labelSizeMap.put("FrontCameraB", 32);
+		labelSizeMap.put("FrontCameraC", 32);
+		labelSizeMap.put("RRearwardLookingSideCamera", 32);
+		labelSizeMap.put("LRearwardLookingSideCamera", 32);
+		labelSizeMap.put("RForwardLookingSideCamera", 32);
+		labelSizeMap.put("LForwardLookingSideCamera", 32);
+		labelSizeMap.put("RearviewCamera", 32);
+		labelSizeMap.put("FrontUltrasonicA", 16);
+		labelSizeMap.put("FrontUltrasonicB", 16);
+		labelSizeMap.put("FrontUltrasonicC", 16);
+		labelSizeMap.put("FrontUltrasonicD", 16);
+		labelSizeMap.put("FrontUltrasonicE", 16);
+		labelSizeMap.put("FrontUltrasonicF", 16);
+		labelSizeMap.put("RearUltrasonicA", 16);
+		labelSizeMap.put("RearUltrasonicB", 16);
+		labelSizeMap.put("RearUltrasonicC", 16);
+		labelSizeMap.put("RearUltrasonicD", 16);
+		labelSizeMap.put("RearUltrasonicE", 16);
+		labelSizeMap.put("RearUltrasonicF", 16);
+		labelSizeMap.put("FrontRadar", 32);
 		
 		addLabels(model, factory, labelSizeMap);
 	}
