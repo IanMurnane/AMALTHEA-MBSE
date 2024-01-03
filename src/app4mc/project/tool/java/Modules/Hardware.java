a617185913ad59a03184fe9434fb0283a9a8deeb
Ian Murnane <ian.murnane@fh-dortmund.de>
2024-01-03 01:15:22 +0100

    added frequency domain definitions

--- src/app4mc/project/tool/java/Modules/Hardware.java
+++ src/app4mc/project/tool/java/Modules/Hardware.java
@@ -6,6 +6,9 @@ import org.eclipse.app4mc.amalthea.model.Amalthea;
 import org.eclipse.app4mc.amalthea.model.AmaltheaFactory;
 import org.eclipse.app4mc.amalthea.model.DataSize;
 import org.eclipse.app4mc.amalthea.model.DataSizeUnit;
+import org.eclipse.app4mc.amalthea.model.Frequency;
+import org.eclipse.app4mc.amalthea.model.FrequencyDomain;
+import org.eclipse.app4mc.amalthea.model.FrequencyUnit;
 import org.eclipse.app4mc.amalthea.model.MemoryDefinition;
 import org.eclipse.app4mc.amalthea.model.ProcessingUnitDefinition;
 
@@ -18,6 +21,9 @@ public class Hardware {
 		createMemoryDefinition(model, factory, "RAM12", 12);
 		createMemoryDefinition(model, factory, "RAM16", 16);
 		createMemoryDefinition(model, factory, "RAM32", 32);
+
+		createFrequencyDomain(model, factory, "GHz1_5", 1.5);
+		createFrequencyDomain(model, factory, "GHz2_0", 2);
 	}
 
 	private static void createProcessingUnitDefinition(Amalthea model, AmaltheaFactory factory, String name) {
@@ -34,5 +40,15 @@ public class Hardware {
 		ram.setSize(dataSize);
 		ram.setName(name);
 		model.getHwModel().getDefinitions().add(ram);
+	}
+
+	private static void createFrequencyDomain(Amalthea model, AmaltheaFactory factory, String name, double frequency) {
+		FrequencyDomain frequencyDomain = factory.createFrequencyDomain();
+		Frequency freq = factory.createFrequency();
+		freq.setValue(frequency);
+		freq.setUnit(FrequencyUnit.GHZ);
+		frequencyDomain.setDefaultValue(freq);
+		frequencyDomain.setName(name);
+		model.getHwModel().getDomains().add(frequencyDomain);
 	}
 }
